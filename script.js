d585f5705ea63ecf249cd422167d67b4b84e0767
yiliasm <yiliasm@gmail.com>
2025-10-03 12:14:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-function rollDice() {
-    for (let i = 1; i <= 5; i++) {
-        //random roll 1-6
-        let roll = Math.floor(Math.random() *6) +1;
-        document.getElementById("die" + i).value = roll;
+const API_HOST = "dicerollerapi-bgcgg6fwbzcebwdf.eastus-01.azurewebsites.net";
+
+//asynchronous function to roll dice through calling the api
+async function rollDice() {
+    try {
+        const response = await fetch(`${API_HOST}/roll?count=5`);
+        const data = await response.json();
+
+        //loop through each die and update
+        for (let i = 1; i <= 5; i++) {
+            document.getElementById(`die${i}`).value = data.rolls[i - 1];
+        }
+    } catch (error) {
+        //catch for cors or an api failure
+        console.error("CORS or nertwork error:", error);
+        alert ("Eroor calling the API, check the console for details.");
     }
 
-    //autofocus the enter button to keep rolling
+    //autofocus enter button to keep rolling
     document.getElementById("rollButton").focus();
+
 }
+
+//wake up the seerver
+window.onload = async () => {
+    try {
+        await fetch(`${API_HOST}/wake`);
+        rollDice();
+
+    //catch for if the server fails to wake up
+    } catch (e) {
+        console.error("Wake failed", e);
+    }
+};
